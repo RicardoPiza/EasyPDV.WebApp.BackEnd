f3b36c40f157045a2020f1e0ae402faf25b9eace
RicardoPiza <pizricardo@gmail.com>
2023-04-03 20:23:16 -0300

    v0.0.1

--- EasyPDV.BackEnd.Domain/Services/ProductService.cs
+++ EasyPDV.BackEnd.Domain/Services/ProductService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EasyPDV.BackEnd.Domain.Interfaces.Repositories
 {
-    internal interface IProductRepository
+    internal class ProductService
     {
     }
 }
